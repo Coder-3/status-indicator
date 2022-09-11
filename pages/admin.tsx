b054b5d783bae9191e6c579b38393811394b07a2
Cedric <cedric@liquid-state.com>
2022-09-11 11:06:00 +1000

    Added CRUD functionality for the facilities and split code into components and improved the data hierarchy

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,25 +1,50 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Table, Modal, Button, Group, TextInput, Select } from "@mantine/core";
 import { supabase } from "../utils/supabaseClient";
 
-const RowActionButtons = ({
-  id,
-  setCurrentFacilityId,
-  setIsModalOpen,
-}: {
+type Facility = {
   id: string;
-  setCurrentFacilityId: (id: string) => void;
+  name: string;
+  status: string;
+  notes: string;
+  edit: any;
+};
+
+const RowActionButtons = ({
+  facility,
+  setCurrentFacility,
+  setIsModalOpen,
+  getFacilities,
+}: {
+  facility: Facility | null;
+  setCurrentFacility: (facility: Facility | null) => void;
   setIsModalOpen: (isModalOpen: boolean) => void;
+  getFacilities: () => void;
 }) => {
-  const handleDelete = () => {
-    console.log("Delete", id);
+  const handleDelete = async () => {
+    const user = supabase.auth.user();
+    if (!user) return;
+
+    if (facility && facility.id) {
+      try {
+        await supabase
+          .from("facilities")
+          .update({ deleted: true })
+          .eq("id", facility.id);
+      } catch (error: any) {
+        console.error(error);
+      }
+    }
+    getFacilities();
   };
 
   const handleEdit = () => {
-    setCurrentFacilityId(id);
-    setIsModalOpen(true);
+    if (facility) {
+      setCurrentFacility(facility);
+      setIsModalOpen(true);
+    }
   };
 
   return (
@@ -34,92 +59,71 @@ const RowActionButtons = ({
   );
 };
 
-const AddEditFacilityModal = ({ id }: { id: string | null }) => {
-  const [name, setName] = useState("");
-  const [status, setStatus] = useState<string | null>(null);
-  const [notes, setNotes] = useState("");
+const AddEditFacilityModal = ({
+  facility,
+  setIsModalOpen,
+  getFacilities,
+}: {
+  facility: Facility | null;
+  setIsModalOpen: (isModalOpen: boolean) => void;
+  getFacilities: () => void;
+}) => {
+  const [name, setName] = useState(facility ? facility.name : "");
+  const [status, setStatus] = useState<string | null>(
+    facility ? facility.status : null
+  );
+  const [notes, setNotes] = useState(facility ? facility.notes : "");
 
-  const handleSave = () => {
-    if (id) {
-      console.log("editing", id, name, status, notes);
-    } else {
-      console.log("adding", name, status, notes);
-    }
+  const handleSave = async (e: any) => {
+    e.preventDefault();
+
+    const user = supabase.auth.user();
+    if (!user) return;
+
+    const newFacility: any = {
+      id: facility ? facility.id : undefined,
+      name,
+      status,
+      notes,
+    };
+
+    await supabase.from("facilities").upsert(newFacility);
+
+    setIsModalOpen(false);
+    getFacilities();
   };
 
   return (
     <>
-      <TextInput
-        label="Name"
-        placeholder="Name"
-        value={name}
-        onChange={(event) => setName(event.currentTarget.value)}
-      />
-      <Select
-        data={["Open", "Closed"]}
-        label="Status"
-        placeholder="Status"
-        searchable
-        clearable
-        value={status}
-        onChange={setStatus}
-      />
-      <TextInput
-        label="Notes"
-        placeholder="Notes"
-        value={notes}
-        onChange={(event) => setNotes(event.currentTarget.value)}
-      />
+      <form onSubmit={handleSave}>
+        <TextInput
+          label="Name"
+          placeholder="Name"
+          value={name}
+          onChange={(event) => setName(event.currentTarget.value)}
+        />
+        <Select
+          data={["Open", "Closed"]}
+          label="Status"
+          placeholder="Status"
+          searchable
+          clearable
+          value={status}
+          onChange={setStatus}
+        />
+        <TextInput
+          label="Notes"
+          placeholder="Notes"
+          value={notes}
+          onChange={(event) => setNotes(event.currentTarget.value)}
+        />
+        <Button type="submit">Save</Button>
+      </form>
     </>
   );
 };
 
-type Facility = {
-  id: string;
-  name: string;
-  status: string;
-  notes: string;
-  edit: any;
-};
-
-const Admin: NextPage = () => {
-  const [currentFacilityId, setCurrentFacilityId] = useState<string | null>(
-    null
-  );
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [tableData, setTableData] = useState<Facility[]>([]);
-
-  const getFacilities = async () => {
-    try {
-      const { data, error } = await supabase.from("facilities").select("*");
-      if (data && data.length > 0) {
-        const facilitiesAsTableData = data.map((facility: Facility) => {
-          return {
-            id: facility.id,
-            name: facility.name,
-            status: facility.status,
-            notes: facility.notes,
-            edit: (
-              <RowActionButtons
-                id={facility.id}
-                setCurrentFacilityId={setCurrentFacilityId}
-                setIsModalOpen={setIsModalOpen}
-              />
-            ),
-          };
-        });
-        setTableData(facilitiesAsTableData);
-      }
-      if (error) throw error;
-    } catch (error: any) {
-      alert(error.error_description || error.message);
-    }
-  };
-
-  useEffect(() => {
-    getFacilities();
-  }, []);
-
+const FacilitiesTable = ({ tableData }: { tableData: Facility[] }) => {
   const rows = tableData.map((facility) => (
     <tr key={facility.id}>
       <td>{facility.name}</td>
@@ -128,6 +132,70 @@ const Admin: NextPage = () => {
       <td>{facility.edit}</td>
     </tr>
   ));
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Status</th>
+          <th>Notes</th>
+          <th>Edit</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </Table>
+  );
+};
+
+const Admin: NextPage = () => {
+  const [facilities, setFacilities] = useState<Facility[]>([]);
+  const [currentFacility, setCurrentFacility] = useState<Facility | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [tableData, setTableData] = useState<Facility[]>([]);
+
+  const getFacilities = useCallback(async () => {
+    try {
+      const { data, error } = await supabase
+        .from("facilities")
+        .select("*")
+        .eq("deleted", false);
+      if (data && data.length > 0) {
+        setFacilities(data);
+      }
+      if (error) throw error;
+    } catch (error: any) {
+      alert(error.error_description || error.message);
+    }
+  }, [setFacilities]);
+
+  const createTableData = useCallback(() => {
+    const facilitiesAsTableData = facilities.map((facility: Facility) => {
+      return {
+        id: facility.id,
+        name: facility.name,
+        status: facility.status,
+        notes: facility.notes,
+        edit: (
+          <RowActionButtons
+            facility={facility}
+            setCurrentFacility={setCurrentFacility}
+            setIsModalOpen={setIsModalOpen}
+            getFacilities={getFacilities}
+          />
+        ),
+      };
+    });
+    setTableData(facilitiesAsTableData);
+  }, [getFacilities, facilities, setTableData]);
+
+  useEffect(() => {
+    getFacilities();
+  }, [getFacilities]);
+
+  useEffect(() => {
+    createTableData();
+  }, [facilities, createTableData]);
 
   return (
     <>
@@ -141,32 +209,25 @@ const Admin: NextPage = () => {
         <Modal
           opened={isModalOpen}
           onClose={() => setIsModalOpen(false)}
-          title={currentFacilityId ? "Edit Facility" : "Add Facility"}
+          title={currentFacility ? "Edit Facility" : "Add Facility"}
         >
-          <AddEditFacilityModal id={currentFacilityId} />
+          <AddEditFacilityModal
+            facility={currentFacility}
+            setIsModalOpen={setIsModalOpen}
+            getFacilities={getFacilities}
+          />
         </Modal>
-
+        <FacilitiesTable tableData={tableData} />
         <Group position="center">
           <Button
             onClick={() => {
-              setCurrentFacilityId(null);
+              setCurrentFacility(null);
               setIsModalOpen(true);
             }}
           >
             Add Facility
           </Button>
         </Group>
-        <Table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Status</th>
-              <th>Notes</th>
-              <th>Edit</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </Table>
       </main>
     </>
   );
